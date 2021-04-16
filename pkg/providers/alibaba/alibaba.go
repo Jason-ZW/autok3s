44d9b470d34cb1b8f187248e517d0990047412f1
Jason-ZW <zhenyang@rancher.com>
2021-04-16 14:20:42 +0800

    fix(cluster): make base.go more compatible
    Signed-off-by: Jason-ZW <zhenyang@rancher.com>

--- pkg/providers/alibaba/alibaba.go
+++ pkg/providers/alibaba/alibaba.go
@@ -144,14 +144,14 @@ func (p *Alibaba) CreateK3sCluster() (err error) {
 	if p.SSHUser == "" {
 		p.SSHUser = defaultUser
 	}
-	return p.InitCluster(p.Options, p.GenerateManifest, p.generateInstance)
+	return p.InitCluster(p.Options, p.GenerateManifest, p.generateInstance, nil)
 }
 
 func (p *Alibaba) JoinK3sNode() (err error) {
 	if p.SSHUser == "" {
 		p.SSHUser = defaultUser
 	}
-	return p.JoinNodes(p.generateInstance, func() error { return nil })
+	return p.JoinNodes(p.generateInstance, func() error { return nil }, false)
 }
 
 func (p *Alibaba) Rollback() error {
